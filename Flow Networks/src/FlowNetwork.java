0021742292fdf1ba284cd1ef1187d29438ea3ea9
Hasal Fernando <witty.leo98@gmail.com>
2019-03-18 11:08:17 +0530

    Initial Commit

---
+++ Flow Networks/src/FlowNetwork.java
@@ -0,0 +1,15 @@
+public class FlowNetwork {
+
+    public static void main(String[] args) {
+
+        int numOfNodes = (int)((Math.random() * 7) + 4);
+        int[] arr = new int[numOfNodes];
+        System.out.println(numOfNodes);
+
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = (int)((Math.random() * 16) + 5);
+            System.out.println(arr[i]);
+        }
+
+    }
+}
