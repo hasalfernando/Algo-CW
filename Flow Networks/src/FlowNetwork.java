9b168e49d1636b2f167ee58be5ea5e95aef4cdd3
Hasal Fernando <witty.leo98@gmail.com>
2019-03-20 15:50:10 +0530

    flow network generated with solving the duplicate issue

--- Flow Networks/src/FlowNetwork.java
+++ Flow Networks/src/FlowNetwork.java
@@ -3,36 +3,57 @@ import java.util.List;
 
 public class FlowNetwork {
 
-    public static void main(String[] args) {
+    private static int numOfNodes = (int)((Math.random() * 9) + 4);
+    private static int numOfEdges = (int)((Math.random() * (numOfNodes*(numOfNodes-1))) + 1);
 
-        int numOfNodes = (int)((Math.random() * 7) + 4);
-        int numOfEdges = (int)((Math.random() * (numOfNodes*(numOfNodes-1))) + 1);
-        int[] edge_u = new int[numOfEdges];
-        int[] edge_v = new int[numOfEdges];
-        int[] edge_capacity = new int[numOfEdges];
-        int[] edge_reverse_capacity= new int[numOfEdges];
+    private static int[] nodes = new int[numOfNodes];
+    private static char[] nodeNames = new char[numOfNodes];
+    private static int[] edge_u = new int[numOfEdges];
+    private static int[] edge_v = new int[numOfEdges];
+    private static int[] edge_capacity = new int[numOfEdges];
+    private static int[] edge_reverse_capacity= new int[numOfEdges];
+
+    private static int[][] connected = new int[numOfNodes][numOfNodes];
+
+    public static void main(String[] args) {
 
         System.out.println("Nodes: "+numOfNodes);
         System.out.println("Edges: "+numOfEdges);
-
+/*
+        for(int i=0; i<numOfNodes; i++){
+            nodes[i] = i+1;
+            if(i==0){
+                nodeNames[i]='s';
+            }
+            else if(i==numOfNodes-1){
+                nodeNames[i]='t';
+            }
+            else{
+                nodeNames[i]=(char)(i+96);
+            }
+        }
+*/
         for(int i = 0; i<edge_u.length; i++){
             edge_u[i]=(int)((Math.random() * numOfNodes) + 1);
             int tempVNode;
             do{
                 tempVNode = (int)((Math.random() * numOfNodes) + 1);
-            }while(tempVNode == edge_u[i]);
+                System.out.println(tempVNode);
+            }while((tempVNode == edge_u[i])||(isConnected(edge_u[i],tempVNode)));
+
             edge_v[i]=tempVNode;
             edge_capacity[i] = capacityGenerator();
+            connect(edge_u[i],edge_v[i]);
         }
 
         System.out.println("edge U");
-        for (int anEdge_u : edge_u) {
-            System.out.print(anEdge_u + " ");
+        for (int i = 0; i<edge_u.length; i++) {
+            System.out.print(edge_u[i] + " ");
         }
 
         System.out.println("\nedge V");
-        for (int anEdge_v : edge_v) {
-            System.out.print(anEdge_v + " ");
+        for (int i = 0 ; i<edge_v.length; i++) {
+            System.out.print(edge_v[i] + " ");
         }
 
         System.out.println("\nCapacities");
@@ -46,5 +67,16 @@ public class FlowNetwork {
         return (int) ((Math.random() * 20) + 5);
     }
 
+    private static void connect(int u, int v){
+        connected[u-1][v-1] = 1;
+    }
 
+    private static boolean isConnected(int u, int v){
+        if(connected[u-1][v-1]==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
