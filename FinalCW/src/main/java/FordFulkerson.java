31b9f3bc505ab60de86130b6ba0469e7c46c9794
Hasal Fernando <witty.leo98@gmail.com>
2019-04-06 19:45:57 +0530

    Edited array

--- FinalCW/src/main/java/FordFulkerson.java
+++ FinalCW/src/main/java/FordFulkerson.java
@@ -135,12 +135,12 @@ class FordFulkerson extends JApplet{
 
         // Let us create a graph shown in the above example
         int graph[][] =new int[][] {
-                {0, 16, 13, 0, 0, 0},
-                {0, 0, 10, 12, 0, 0},
-                {0, 4, 0, 0, 14, 0},
-                {0, 0, 9, 0, 0, 20},
-                {0, 0, 0, 7, 0, 4},
-                {0, 0, 0, 0, 0, 0}
+                {0, 16, 13,  0,  0,  0},
+                {0,  0, 10, 12,  0,  0},
+                {0,  4,  0,  0, 14,  0},
+                {0,  0,  9,  0,  0, 20},
+                {0,  0,  0,  7,  0,  4},
+                {0,  0,  0,  0,  0,  0}
         };
 
         FordFulkerson m = new FordFulkerson();
